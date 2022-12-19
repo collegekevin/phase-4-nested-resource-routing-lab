4ad6c66893c95aaf8727a5762a69b32e7600ff50
Kevin Price <collegekevin@gmail.com>
2022-12-18 19:44:38 -0800

    commit tree 8171de0ffa1437c8b29e9af18db8a02aae4e5424

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,34 @@
 class ItemsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found_response
 
   def index
-    items = Item.all
-    render json: items, include: :user
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      items = user.items
+    else
+      items = Item.all
+    end
+      render json: items, include: :user
   end
 
+  def show
+    item = Item.find(params[:id])
+    render json: item
+  end
+
+  def create
+    item = Item.create!(items_params)
+    render json: item, status: :created
+  end
+
+end
+
+private
+
+def items_params
+  params.permit(:name, :description, :price, :user_id)
+end
+
+def not_found_response
+  render json: {error: "Iten not found"}, status: :not_found
 end
